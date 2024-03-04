0df8079591c154f94826fd7e9947736ccc431440
Gikubu <davidndungu8203@gmail.com>
2024-03-04 17:14:18 +0300

    here i create a function to add two numbers.

---
+++ bin/dartfunctions.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  // Task 1 addition
+  int addTwo(int a, int b) {
+    return a + b;
+  }
+}
---
+++ test/dartfunctions_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartfunctions/dartfunctions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
